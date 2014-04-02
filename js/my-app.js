c6d4540fdea7fa1ae6e3a111541956ff9202a0a2
WolfLee <liyuan0228@gmail.com>
2014-04-02 14:21:57 +0800

    make image upload prettier

--- js/my-app.js
+++ js/my-app.js
@@ -4,5 +4,10 @@ var myApp = new Framework7();
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+    //dynamicNavbar: true
 });
+
+var btnUpload = document.getElementById("upload");
+btnUpload.onclick = function(){
+  document.getElementById("uploader").click();
+};

4aeefc305838b7b2ebcc44d7f11bf1d06cb7e655
WolfLee <liyuan0228@gmail.com>
2014-04-08 16:42:34 +0800

    Real invoke

--- js/my-app.js
+++ js/my-app.js
@@ -11,3 +11,13 @@ var btnUpload = document.getElementById("upload");
 btnUpload.onclick = function(){
   document.getElementById("uploader").click();
 };
+
+AV.initialize("gosiknzn1db4o6sdlzjo2ozq17sqhyfl1wdomipneda90bbo", "x6zak5uxsty6jatbxx46al0rfsulj2lxntwpgh5125qr63j5");
+
+var TestObject = AV.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}, {
+  success: function(object) {
+    alert("AVOS Cloud works!");
+  }
+});

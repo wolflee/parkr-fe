d7b4a8f1be526f8790380a543965edc7ef53c6b1
WolfLee <liyuan0228@gmail.com>
2014-04-11 12:25:18 +0800

    Fix toolbar animation

--- js/my-app.js
+++ js/my-app.js
@@ -6,44 +6,44 @@ var mainView = myApp.addView('.view-main', {
 
 AV.initialize("gosiknzn1db4o6sdlzjo2ozq17sqhyfl1wdomipneda90bbo", "x6zak5uxsty6jatbxx46al0rfsulj2lxntwpgh5125qr63j5");
 
-function getPrice(){
-  return $("#price .button.active").data("price");
-}
-
-function getFeatures(){
-  return AV._.toArray(
-    $("#feature .button.active").map(function(){
-    return $(this).data("feature");
-  }));
-}
-
-function getPhotoFile(){
-  var fileUpload = $("#fileUpload")[0];
-  if (fileUpload.files.length > 0) {
-    var file = fileUpload.files[0];
-    var name = "photo.jpg";
-
-    var avFile = new AV.File(name, file);
-    return avFile;
-  }
-  return null;
-}
-
-function getName(){
-  return $("#textLocation").val() ? $("#textLocation").val() : "";
-}
-
-function getGeoLocation(){
-  navigator.geolocation.getCurrentPosition(function(position){
-    $("#hiddenLocation").data("latitude", position.coords.latitude);
-    $("#hiddenLocation").data("longitude", position.coords.longitude);
-  });
-}
-
-
 $(document).ready(function(){
 
   FastClick.attach(document.body);
+
+  function getPrice(){
+    return $("#price .button.active").data("price");
+  }
+
+  function getFeatures(){
+    return AV._.toArray(
+      $("#feature .button.active").map(function(){
+      return $(this).data("feature");
+    }));
+  }
+
+  function getPhotoFile(){
+    var fileUpload = $("#fileUpload")[0];
+    if (fileUpload.files.length > 0) {
+      var file = fileUpload.files[0];
+      var name = "photo.jpg";
+
+      var avFile = new AV.File(name, file);
+      return avFile;
+    }
+    return null;
+  }
+
+  function getName(){
+    return $("#textLocation").val() ? $("#textLocation").val() : "";
+  }
+
+  function getGeoLocation(){
+    navigator.geolocation.getCurrentPosition(function(position){
+      $("#hiddenLocation").data("latitude", position.coords.latitude);
+      $("#hiddenLocation").data("longitude", position.coords.longitude);
+    });
+  }
+
 
   $("#buttonSubmit").on('click', function(){
     var price = getPrice();
@@ -89,15 +89,18 @@ $(document).ready(function(){
   $("#price a").on('click', function(){
     $("#price a").removeClass("active");
     $(this).addClass("active");
-
-    if($("#toolbar").hasClass("beneath")){
-      $("#toolbar").animate({
-        "bottom": "+=50px"
-      }, 1000, function(){
-        $("#toolbar").removeClass('beneath');
-      });
-    }
   });
+
+  $("#price a").on('click', moveToolbar);
+
+  function moveToolbar(){
+    $("#price a").off('click', moveToolbar);
+    $("#toolbar").animate({
+      "bottom": "+=50px"
+    }, 1000, function(){
+      $("#toolbar").removeClass('beneath');
+    });
+  }
 
   $("#feature .required a").on('click', function(){
     $("#feature .required a").removeClass("active");

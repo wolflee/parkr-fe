e0abfbb1591d3896047c3ccb0274c19ca6f89d9c
WolfLee <liyuan0228@gmail.com>
2014-04-16 00:02:43 +0800

    Finalize image resizing and uploading
    with megapixelimage to deal with image resizing and output base64 bytes
    for uploading; and exif to fix the orientation problem on iOS.

--- js/my-app.js
+++ js/my-app.js
@@ -4,25 +4,6 @@ var mainView = myApp.addView('.view-main', {
   dynamicNavbar: true
 });
 
-function convertImgToBase64(url, callback, outputFormat){
-  var canvas = document.createElement('CANVAS'),
-  ctx = canvas.getContext('2d'),
-  img = new Image;
-  img.crossOrigin = 'Anonymous';
-  img.onload = function(){
-    canvas.height = img.height;
-    canvas.width = img.width;
-    ctx.drawImage(img,0,0);
-    var dataURL = canvas.toDataURL(outputFormat || 'image/png');
-    callback.call(this, dataURL);
-    canvas = null; 
-  };
-  img.src = url;
-}
-
-function isiOSDevice(){
-  return !!(navigator.userAgent.match(/(iPod|iPhone|iPad)/));
-}
 AV.initialize("gosiknzn1db4o6sdlzjo2ozq17sqhyfl1wdomipneda90bbo", "x6zak5uxsty6jatbxx46al0rfsulj2lxntwpgh5125qr63j5");
 
 $(document).ready(function(){
@@ -35,11 +16,13 @@ $(document).ready(function(){
     $("input").val(null);
     $("#buttonUpload span").show();
     $("#buttonUpload img").hide();
-    //$("#buttonUpload img").remove();
-    //$("#buttonUpload canvas").remove();
     $("#price a").on('click', moveToolbar);
     $("#buttonUpload").addClass('button button-big button-camera');
     $("#buttonUpload").removeClass('preview');
+  }
+
+  function isiOSDevice(){
+    return !!(navigator.userAgent.match(/(iPod|iPhone|iPad)/));
   }
 
   function isCanvasSupported(){
@@ -62,8 +45,6 @@ $(document).ready(function(){
     var fileUpload = $("#fileUpload")[0];
     if (fileUpload.files.length > 0) {
       var name = "photo.jpg";
-      //var dataBase64 = $("#imgUpload").data("base64");
-
       var dataBase64 = $("#imgUpload")[0].src.split(",")[1];
       var avFile = new AV.File(name, { base64: dataBase64 });
       return avFile;
@@ -184,82 +165,19 @@ $(document).ready(function(){
 
     var scaledImage = $("#imgUpload")[0];
     if(isiOSDevice()) {
-      var orientation;
       EXIF.getData(e.target.files[0], function(){
-        //$("#imageUpload").data("orientation") = EXIF.getTag(this, "Orientation");
         var orientation = EXIF.getTag(this, "Orientation");
         mpImg.render(scaledImage, { maxWidth: 400, maxHeight: 400, quality: 1.0, orientation: orientation });
       });
-      //alert(orientation);
     } else {
-      mpImg.render(scaledImage, { maxWidth: 400, maxHeight: 400, quality: 1.0, orientation: 0  });
+      mpImg.render(scaledImage, { maxWidth: 400, maxHeight: 400, quality: 1.0 });
     }
 
     $("#buttonUpload").removeClass('button button-big button-camera')
     $("#buttonUpload span").hide();
-    //$("#buttonUpload img").remove();
-    //$("#buttonUpload canvas").remove();
     $("#buttonUpload")[0].appendChild(scaledImage);
     $("#buttonUpload").addClass('preview');
     $("#imgUpload").show();
-
-    //EXIF.getData(e.target.files[0], function() {
-        //alert(EXIF.pretty(this));
-    //});
-
-    //$("#imgUpload").data("base64", $("#imgUpload")[0].src.split(",")[1]);
-
-    //var loadingImage = loadImage(
-      //e.target.files[0],
-      //function(img){
-        //$("#buttonUpload").removeClass('button button-big button-camera')
-        //$("#buttonUpload span").hide();
-        //$("#buttonUpload img").remove();
-        //$("#buttonUpload canvas").remove();
-        //var scaledImage = loadImage.scale(
-          //img,
-          //{
-            //maxWidth: 400,
-            //maxHeight: 400
-          //}
-        //);
-        //scaledImage.id = "imgUpload";
-        //$("#buttonUpload")[0].appendChild(scaledImage);
-        //$("#buttonUpload").addClass('preview');
-        ////var base64 = scaledImage.toDataURL("image/jpeg").split(",")[1];
-        ////$("#imgUpload").data("base64", base64);
-      //},
-      //{
-        ////noRevoke: true//,
-        ////canvas: true
-      //}
-    //);
-    //var loadingImage = loadImage(
-      //e.target.files[0],
-      //function(img){
-        //$("#buttonUpload").removeClass('button button-big button-camera')
-        //$("#buttonUpload span").hide();
-        //$("#buttonUpload img").remove();
-        //$("#buttonUpload canvas").remove();
-        //var scaledImage = loadImage.scale(
-          //img,
-          //{
-            //maxWidth: 400,
-            //maxHeight: 400
-          //}
-        //);
-        //scaledImage.id = "imgUpload";
-        //$("#buttonUpload")[0].appendChild(scaledImage);
-        //$("#buttonUpload").addClass('preview');
-        //var base64 = scaledImage.toDataURL("image/jpeg").split(",")[1];
-        //$("#imgUpload").data("base64", base64);
-      //},
-      //{
-        //canvas: true
-      //}
-    //);
-    //if (!loadingImage){
-    //}
   });
 
   getGeoLocation();
